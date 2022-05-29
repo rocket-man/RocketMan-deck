45734bbb1dedb6f2f0b993c160ebd69f3a7d2238
Arnab Halder <arnabhalder164@gmail.com>
2022-05-29 12:50:21 +0530

    Create wssetup.sh

---
+++ wssetup.sh
@@ -0,0 +1,15 @@
+##Docker images setup
+sudo docker pull node:latest
+sudo docker pull python:3.9.13-slim-bullseye
+sudo docker pull amazon/aws-lambda-python:latest
+sudo docker pull amazon/aws-cli
+sudo docker pull mcr.microsoft.com/azure-cli
+sudo docker pull centos
+sudo docker pull alpine
+sudo docker pull kalilinux/kali-rolling
+sudo docker pull google/cloud-sdk:latest
+sudo docker pull bitnami/kubectl:latest
+sudo docker pull docker pull openjdk:jdk-buster
+sudo docker pull mariadb:latest
+
+##Github setup
